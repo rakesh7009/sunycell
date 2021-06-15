38e8c0e62611642a3331c1e0bef915bd15473beb
Rakesh Choudhary <67330317+rakesh7009@users.noreply.github.com>
2021-06-15 17:18:51 -0400

    Updated features.py

--- sunycell/features.py
+++ sunycell/features.py
@@ -14,6 +14,8 @@ def get_polygon_from_pts(pts):
         Y = pt[1]
         point_list = [(x,y) for x,y in zip(X,Y)]
         if len(point_list) < 3:
+            polygons.append(none)
+            print(f'WARNING: could not find appropriate number of points i.e 3U')
             continue
         poly = Polygon(point_list)
         polygons.append(poly)
